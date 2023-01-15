3070d9f2db02a8cf4b312f5875ebd53d7db4f8bd
Davis Sylvester <davis@sylvesterllc.com>
2023-01-15 11:08:39 -0600

    Updating

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   title: 'SERIAL NUMBER RECOGNITION',
-  videoFeed: "../../../assets/videos/demo.mp4", // "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
-};
+  videoFeed: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+  appLogo: "../../../assets/images/logo.svg",
+}
